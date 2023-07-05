c40c9babbd0819b5dc051ea0d7402aff0659f8f0
BrenoBar51 <brenobarcelos9@gmail.com.com>
2023-07-05 08:43:01 -0300

    icons

---
+++ vite-project/src/components/Icon.jsx
@@ -0,0 +1,7 @@
+export default function icon({children}){
+    return(
+        <span className="w-7 h-7 flex justify-center items-center rounded-full border border-white">
+            {children}
+        </span>
+    )
+}
---
+++ vite-project/src/components/ListIcon.jsx
@@ -0,0 +1,18 @@
+export default function ListIcon(){
+    return(
+        <div className="flex justify-between gap-5">
+        <span className="w-7 h-7 flex justify-center cursor-pointer rounded-full border border-white">
+            <box-icon type='logo' name='facebook-square' color="#FFFFFF"></box-icon>
+        </span>
+        <span className=" w-7 h-7 flex justify-center cursor-pointer rounded-full border border-white">
+            <box-icon name='instagram' type='logo' color="#FFFFFF"></box-icon>
+        </span>
+        <span className=" w-7 h-7 flex justify-center cursor-pointer rounded-full border border-white">
+            <box-icon name='twitter' type='logo' color="#FFFFFF"></box-icon>
+        </span>
+        <span className=" w-7 h-7 flex justify-center cursor-pointer rounded-full border border-white">
+            <box-icon name='linkedin-square' type='logo' color="#FFFFFF"></box-icon>
+        </span>
+    </div>
+    )
+}
